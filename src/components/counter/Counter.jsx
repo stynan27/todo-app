da063e62b080a50a95912e515e7b2be13da69827
stynan27 <seamustynan16@gmail.com>
2023-04-05 14:20:38 -0400

    Basic Styling

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,4 +1,18 @@
+import './Counter.css';
+
 export default function Counter() {
+
+  // JSX Embedded styling
+  // Instead opt for separate Counter.css file
+  // const buttonStyle = {
+  //   fontSize: "16px",
+  //   color: "white",
+  //   backgroundColor: "#00a5ab",
+  //   width: "100px",
+  //   margin: "10px",
+  //   padding: "15px",
+  //   borderRadius: "30px",
+  // };
 
   function incrementCounterFunction() {
     console.log('increment clicked');
@@ -6,10 +20,20 @@ export default function Counter() {
 
   return (
     <div className="Counter">
-      <span className="count">0</span>
-      <div>
-        <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
-      </div>
+        <span className="count">0</span>
+            <div>
+                {/* Example of style css object with camel-case fontSize
+                and double curly braces i.e. {{}} */}
+                {/* <button className="counterButton" 
+                        onClick={incrementCounterFunction}
+                        style={buttonStyle}
+                > */}
+                <button className="counterButton" 
+                        onClick={incrementCounterFunction}
+                >
+                    +1
+                </button>
+            </div>
     </div>
   )
 }
