53a5f44f9e79cf7f8ce00511e26c91fed90872c3
stynan27 <seamustynan16@gmail.com>
2023-04-06 13:47:31 -0400

    Added state to Count

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,39 +1,45 @@
+import React, { useState } from 'react';
 import './Counter.css';
 
 export default function Counter() {
 
-  // JSX Embedded styling
-  // Instead opt for separate Counter.css file
-  // const buttonStyle = {
-  //   fontSize: "16px",
-  //   color: "white",
-  //   backgroundColor: "#00a5ab",
-  //   width: "100px",
-  //   margin: "10px",
-  //   padding: "15px",
-  //   borderRadius: "30px",
-  // };
+    // JSX Embedded styling
+    // Instead opt for separate Counter.css file
+    // const buttonStyle = {
+    //   fontSize: "16px",
+    //   color: "white",
+    //   backgroundColor: "#00a5ab",
+    //   width: "100px",
+    //   margin: "10px",
+    //   padding: "15px",
+    //   borderRadius: "30px",
+    // };
 
-  function incrementCounterFunction() {
-    console.log('increment clicked');
-  }
+    // [0, f()]
+    //const state = useState(0);
+    //instead write...
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="Counter">
-        <span className="count">0</span>
-            <div>
-                {/* Example of style css object with camel-case fontSize
-                and double curly braces i.e. {{}} */}
-                {/* <button className="counterButton" 
-                        onClick={incrementCounterFunction}
-                        style={buttonStyle}
-                > */}
-                <button className="counterButton" 
-                        onClick={incrementCounterFunction}
-                >
-                    +1
-                </button>
-            </div>
-    </div>
-  )
+    function incrementCounterFunction() {
+        setCount(count+1);
+    }
+
+    return (
+        <div className="Counter">
+            <span className="count">{count}</span>
+                <div>
+                    {/* Example of style css object with camel-case fontSize
+                    and double curly braces i.e. {{}} */}
+                    {/* <button className="counterButton" 
+                            onClick={incrementCounterFunction}
+                            style={buttonStyle}
+                    > */}
+                    <button className="counterButton" 
+                            onClick={incrementCounterFunction}
+                    >
+                        +1
+                    </button>
+                </div>
+        </div>
+    )
 }
