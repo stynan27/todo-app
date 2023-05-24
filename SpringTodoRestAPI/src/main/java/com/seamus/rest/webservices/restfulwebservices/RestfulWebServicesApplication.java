8c5dd3f608cb053270df106b9cb83907d2c5366e
stynan27 <seamustynan16@gmail.com>
2023-05-23 21:57:08 -0400

    Enable CORS with example JS Axios request

--- SpringTodoRestAPI/src/main/java/com/seamus/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ SpringTodoRestAPI/src/main/java/com/seamus/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,12 +2,32 @@ package com.seamus.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+	
+	
+	// Allow Cross Origin Requests
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				// Enable EVERY mapping (endpoint)
+				registry.addMapping("/**")
+					// With ALL request types: GET, PUT, POST
+					.allowedMethods("*")
+					// For specified hostname/origin
+					.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- src/components/todo/welcome/WelcomeComponent.jsx
+++ src/components/todo/welcome/WelcomeComponent.jsx
@@ -1,11 +1,27 @@
 import { useParams, Link } from "react-router-dom";
+import axios from 'axios';
 
 export default function WelcomeComponent() {
 
     // get URL path params by object deconstruction
     const { username }= useParams();
 
-    console.log(username);
+    function callHelloWorldRestAPI(){
+        console.log('called');
+        //axios call to invoke Spring Boot Rest API
+        axios.get("http://localhost:8080/hello-world")
+            .then((resp) => successfulResponse(resp))
+            .catch((error) => errorResponse(error))
+            .finally( () => console.log('cleanup'));
+    }
+
+    function successfulResponse(response) {
+        console.log(response);
+    }
+
+    function errorResponse(error) {
+        console.log(error);
+    }
 
     return (
         <div className="WelcomeComponent">
@@ -13,6 +29,11 @@ export default function WelcomeComponent() {
             <div>
                 {/* Use of Link here to prevent page refresh */}
                 Manage your todos - <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestAPI}>
+                    Call Hello World
+                </button>
             </div>
         </div>
     );
