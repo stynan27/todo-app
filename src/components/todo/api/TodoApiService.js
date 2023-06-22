3c999e22d6ebf76870c3920b9117c5e1cba5addf
stynan27 <seamustynan16@gmail.com>
2023-06-22 15:27:56 -0400

    API update and create TODOS

--- src/components/todo/api/TodoApiService.js
+++ src/components/todo/api/TodoApiService.js
@@ -18,3 +18,10 @@ export const retrieveTodoApi
 
 export const deleteTodoApi 
     = (username, id) => apiClient.delete(`/users/${username}/todos/${id}`);
+
+export const updateTodoApi 
+    = (username, id, todo) => apiClient.put(`/users/${username}/todos/${id}`, todo)
+
+export const createTodoApi 
+    = (username, id) => apiClient.post(`/users/${username}/todos/${id}`);
+
